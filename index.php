b541de07ef3f5ab6730a9b2590f398dda388df85
Vincent Simard <vincent@vincentsimard.com>
2013-02-11 12:30:29 -0500

    Removing old files

--- index.php
+++ index.php
@@ -7,18 +7,14 @@
 
 		<meta name="author" content="Vincent Simard" />
 		<meta name="description" content="The personal website of Vincent Simard" />
-		<meta name="keywords" content="Vincent Simard, Vincent, Simard, Web developer, Web development, Montreal" />
+		<meta name="keywords" content="Vincent Simard, Web developer, Web development, Montreal" />
 
 		<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script><![endif]-->
 		
 		<link rel="stylesheet" href="css/reset.css" media="all" type="text/css" />
 		<link rel="stylesheet" href="css/screen.css" media="screen" type="text/css" />
 		<link rel="stylesheet" href="css/home.css" media="screen" type="text/css" />
-		<!-- Should be using something like [if lte IE 7] but hey... it's my website. -->
-		<!--[if IE]><link rel="stylesheet" href="css/screen-ie.css" media="screen" type="text/css" /><![endif]-->
-		
-		<!-- No scripts at the moment. kthx -->
-		<!-- Except for analytics... -->
+
 		<script type="text/javascript">
 			var _gaq = _gaq || [];
 			_gaq.push(['_setAccount', 'UA-347560-1']);
@@ -34,7 +30,6 @@
 	</head>
 	<body id="page-home">
 		<div id="document" class="container_12">
-			<div id="ala-banner"></div>
 			<header>
 				<figure class="avatar">
 					<img src="media/avatar-kung_pow_52x52.jpg" alt="*this is not my actual photo" class="frame_60x60" />
@@ -48,19 +43,6 @@
 					<p class="telephone">+1 (514) 632.3597</p>
 					<p class="email"><a href="mailto:vincent@vincentsimard.com">vincent@vincentsimard.com</a></p>
 				</section><!-- End .contact -->
-				<nav>
-					<ul class="mainnav">
-						<li><a href="/" title="Home" class="active">Home</a></li>
-						<li><a href="#" title="About">About</a></li>
-						<li><a href="#" title="Portfolio">Portfolio</a></li>
-						<li><a href="#" title="Sandbox">Sandbox</a></li>
-						<li><a href="mailto:vincent@vincentsimard.com" title="Contact">Contact</a></li>
-					</ul>
-					<ul class="lang">
-						<li><a href="/" title="English"><abbr title="English">En</abbr></a></li>
-						<li><a href="#" title="Français"><abbr title="Français">Fr</abbr></a></li>
-					</ul>
-				</nav><!-- End nav -->
 			</header><!-- End header -->
 			<!-- Look ma', no script! -->
 			<section id="hero-container">
@@ -109,141 +91,6 @@
 				</div><!-- End #hero_2 -->
 				</div><!-- End #hero_1 -->
 			</section><!-- End #hero-container -->
-			<section class="content" id="home-whowhathow">
-				<section id="who" class="grid_3">
-					<header>
-						<hgroup>
-							<h2><a href="#" title="About Vincent Simard">Who?</a></h2>
-							<h3>About me</h3>
-						</hgroup>
-					</header>
-					<p>It's in the title—I'm Vincent Simard, a web developer living in Montreal.</p>
-					<p>I'm also a chess-loving hockey player, cycling enthusiast and web junkie.</p>
-					<p class="more"><a href="#" title="Learn more about Vincent">Learn more</a></p>
-				</section><!-- End #who -->
-				<section id="what" class="grid_3">
-					<header>
-						<hgroup>
-							<h2><a href="#" title="Vincent Simard's portfolio">What?</a></h2>
-							<h3>Current projects</h3>
-						</hgroup>
-					</header>
-					<div class="portfolio callonme">
-						<img src="media/portfolio-pp00_52x52.png" alt="PowerPlay00" class="frame_60x60" />
-						<h4>PowerPlay00</h4>
-						<p>Redesign & <a href="http://www.alistapart.com/articles/redesignrealign/" title="Good Designers Redesign, Great Designers Realign">realign</a></p>
-						<p class="date">June 2010</p>
-					</div>
-					<div class="portfolio toffee">
-						<img src="media/portfolio-toffee_52x52.png" alt="Call On Me" class="frame_60x60" />
-						<h4>Your website</h4>
-						<p>This might be your project</p>
-						<p class="date">Sept. 2010</p>
-					</div>
-					<p class="more"><a href="#" title="Vincent Simard's online portfolio">Explore the portfolio</a></p>
-				</section><!-- End #what -->
-				<section id="how" class="grid_6">
-					<header>
-						<hgroup>
-							<h2><a href="#" title="">Hrm?</a></h2>
-							<h3>Some <em>stuff</em> goes here</h3>
-						</hgroup>
-						<p class="info">Maybe.</p>
-					</header>
-					<p>There was supposed to be content here but I accidentally the whole thing.</p>
-					<!-- <p class="more"><a href="#" title="Browse">oops</a></p> -->
-				</section><!-- End #how -->
-			</section>
-			<section id="home-samples">
-				<h2>Featured work from the portfolio</h2>
-				<figure>
-					<img src="media/sample_1.jpg" alt="Pinkmount Poker Tour 2009" />
-					<figcaption>
-						<a href="http://vincentsimard.com/poker/">
-							<em>Pinkmount Poker Tour 2009</em>
-							<br />
-							Event promotion
-						</a>
-					</figcaption>
-				</figure>
-				<figure>
-					<img src="media/sample_2.jpg" alt="Powerplay00" />
-					<figcaption>
-						<a href="http://vincentsimard.com/powerplay00/">
-							<em>Powerplay00</em>
-							<br />
-							Hockey league management
-							<br />
-							<small>Under construction</small>
-						</a>
-					</figcaption>
-				</figure>
-				<figure>
-					<img src="media/sample_3.jpg" alt="Ville de Laval" />
-					<figcaption>
-						<a href="http://vincentsimard.com/wlav/">
-							<em>Ville de Laval</em>
-							<br />
-							Municipal web portal / Intranet
-							<br />
-							<small>Prototype, realigned design for new CMS integration</small>
-						</a>
-					</figcaption>
-				</figure>
-			</section><!-- End #home-samples -->
-			<footer>
-				<div class="endnav">
-					<h2>Vincent Simard</h2>
-					<nav>
-						<ul class="mainnav">
-							<li><a href="/" title="Home" class="active">Home</a></li>
-							<li><a href="#" title="About">About</a></li>
-							<li><a href="#" title="Portfolio">Portfolio</a></li>
-							<li><a href="#" title="Sandbox">Sandbox</a></li>
-							<li><a href="mailto:vincent@vincentsimard.com" title="Contact">Contact</a></li>
-						</ul>
-					</nav><!-- End nav -->
-					<section class="contact">
-						<p class="telephone">+1 (514) 632.3597</p>
-						<p class="email"><a href="mailto:vincent@vincentsimard.com">vincent@vincentsimard.com</a></p>
-					</section><!-- End .contact -->
-				</div>
-				<div class="smallprint">
-					<p>
-						Designed with
-						<?php
-							$designedWith[1] = 'passion';
-							$designedWith[2] = 'love';
-							$designedWith[3] = 'care';
-							echo $designedWith[mt_rand(1, count($designedWith))];
-						?>
-						<span class="ampersand">&</span>
-						developed using Web Standards:
-						<a href="http://validator.w3.org/check/referer">HTML5</a>,
-						<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS3</a>,
-						<a href="http://www.contentquality.com/mynewtester/cynthia.exe?rptmode=-1&amp;url1=http://<?php echo $HTTP_SERVER_VARS['SERVER_NAME'] ?><?php echo $HTTP_SERVER_VARS['SCRIPT_NAME'] ?>">508</a>,
-						<a href="http://www.contentquality.com/mynewtester/cynthia.exe?rptmode=2&amp;url1=http://<?php echo $HTTP_SERVER_VARS['SERVER_NAME'] ?><?php echo $HTTP_SERVER_VARS['SCRIPT_NAME'] ?>">WAI</a>.
-						<br />
-						No
-						<?php
-							$harmed[1] = 'rounded borders';
-							$harmed[2] = '&lt;marquee/&gt; tags';
-							$harmed[3] = '&lt;blink/&gt; tags';
-							$harmed[4] = 'gradients';
-							$harmed[5] = 'reflective objects';
-							$harmed[6] = 'stock photos';
-							echo $harmed[mt_rand(1, count($harmed))];
-						?>
-						were harmed in the making of this website.
-					</p>
-					<p class="copyright">
-						Copyright &copy; 2012 Vincent Simard. All rights reserved.
-						<br />
-						Stealing is bad—don't do it. <em>Hire me</em> instead!
-					</p>
-				</div>
-			</footer>
-			<p id="end"><em>The End.</em> Thank you for visiting.</p>
 		</div><!-- End #document -->
 	</body>
 </html>
